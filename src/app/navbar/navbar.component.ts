ac033ebe4e180bf781d9ffcb5f320a1538006ac2
Joaoof <joaodeus400@gmail.com>
2024-06-18 14:27:45 -0300

    feat: add header responsivo

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-  menuOpen = false;
 
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;
+export class NavbarComponent {
+  @ViewChild('menuMobile') menuMobile: ElementRef | undefined | any;
+  @ViewChild('iconImage') iconImage: ElementRef | undefined | any;
+
+  menuShow() {
+    if (this.menuMobile.nativeElement.classList.contains('open')) {
+      this.menuMobile.nativeElement.classList.remove('open');
+      this.iconImage.nativeElement.src = "../../assets/close_white_36dp.svg"; // Caminho quando o menu está fechado
+    } else {
+      this.menuMobile.nativeElement.classList.add('open');
+      this.iconImage.nativeElement.src = "caminho/para/imagem/aberta.svg"; // Caminho quando o menu está aberto
+    }
   }
 }
